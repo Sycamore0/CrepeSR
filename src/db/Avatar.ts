bee20b7dcafbeff6d3d1f4447eb4365feb5fc670
memetrollsXD <memetrollsxd@gmail.com>
2022-08-01 04:24:03 +0200

    Avatar.ts: get, getAll

--- src/db/Avatar.ts
+++ src/db/Avatar.ts
@@ -5,7 +5,7 @@ import Database from "./Database";
 const c = new Logger("Avatar");
 
 export default class Avatar {
-    protected _id!: number;
+    public readonly _id!: number;
     public avatarType!: AvatarType;
     public ownerUid!: number;
     public baseAvatarId!: number;
@@ -13,17 +13,21 @@ export default class Avatar {
     public level!: number;
     public promotion!: number;
     public rank!: number;
-    public skilltreeList!: AvatarSkillTree[];
-    public equipmentUniqueId!: number;
-    public equipRelicList!: EquipRelic[];
+    public skilltreeList?: AvatarSkillTree[];
+    public equipmentUniqueId?: number;
+    public equipRelicList?: EquipRelic[];
     public hp!: number;
     public sp!: number;
     public satiety!: number;
 
+    private constructor(_fromdb: object) {
+        Object.assign(this, _fromdb);
+    }
+
     public static async fromUID(uid: string | number): Promise<Avatar[]> {
         const db = Database.getInstance();
         const avatars = await db.getAll("avatars", { ownerUid: Number(uid) }) as unknown as Avatar[];
-        if(!avatars){
+        if (!avatars) {
             return await Avatar.create(uid);
         }
         return avatars;
@@ -35,12 +39,12 @@ export default class Avatar {
             ownerUid: Number(uid),
             avatarType: AvatarType.AVATAR_FORMAL_TYPE,
             baseAvatarId: 1001,
-            exp: 0,
+            exp: 25,
             level: 1,
             promotion: 1,
             rank: 1,
             skilltreeList: [],
-            equipmentUniqueId: 13501, //todo: find default weapon(?)
+            equipmentUniqueId: 20000, // EquipmentExcelTable
             equipRelicList: [],
             hp: 1000,
             sp: 1000,
@@ -48,7 +52,19 @@ export default class Avatar {
         } as unknown as Avatar;
 
         await db.set("avatars", dataObj);
-        return [ dataObj ];
+        return [dataObj];
+    }
+
+    public static async get(uid: string | number, baseAvatarId: number): Promise<Avatar> {
+        const db = Database.getInstance();
+        const avatar = await db.get("avatars", { ownerUid: Number(uid), baseAvatarId }) as unknown as Avatar;
+        return avatar;
+    }
+
+    public static async getAll(uid: string | number) {
+        const db = Database.getInstance();
+        const avatars = await db.getAll("avatars", { ownerUid: Number(uid) }) as unknown as Avatar[];
+        return avatars;
     }
 
     public async save() {
