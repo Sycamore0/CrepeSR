a06f36f46b10a0d255d4cf4e94c8b6afc204d1e1
memetrollsXD <memetrollsxd@gmail.com>
2022-08-02 18:51:24 +0200

    login activity

--- src/server/packets/GetLoginActivityCsReq.ts
+++ src/server/packets/GetLoginActivityCsReq.ts
@@ -5,6 +5,10 @@ import Session from "../kcp/Session";
 export default async function handle(session: Session, packet: Packet) {
     session.send("GetLoginActivityScRsp", {
         retcode: 0,
-        loginActivityList: []
+        loginActivityList: [{
+            id: 1001,
+            loginDays: 1,
+            hasTakenLoginActivityRewardDaysList: []
+        }]
     } as GetLoginActivityScRsp);
 }
