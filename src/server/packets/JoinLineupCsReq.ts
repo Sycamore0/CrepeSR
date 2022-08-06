5e1209deba109368967ac4464150b570dee3c854
Dang Hoang Phuc <13364457+phuchptty@users.noreply.github.com>
2022-08-07 02:36:41 +0700

    Fix & implement some lineup stuff (#53)
    * fix: add avatar for each lineup
    
    * feat: switch lineup

--- src/server/packets/JoinLineupCsReq.ts
+++ src/server/packets/JoinLineupCsReq.ts
@@ -1,20 +1,26 @@
-import { AvatarType, JoinLineupCsReq, JoinLineupScRsp, SyncLineupNotify, SyncLineupReason } from "../../data/proto/StarRail";
-import Avatar from "../../db/Avatar";
+import {
+    JoinLineupCsReq,
+    JoinLineupScRsp,
+    SyncLineupNotify,
+    SyncLineupReason
+} from "../../data/proto/StarRail";
 import Packet from "../kcp/Packet";
 import Session from "../kcp/Session";
 
 // JoinLineupCsReq { baseAvatarId: 1002, slot: 1 }
 export default async function handle(session: Session, packet: Packet) {
     const body = packet.body as JoinLineupCsReq;
-    session.send(JoinLineupScRsp, { retcode: 0 });
+    session.send(JoinLineupScRsp, {retcode: 0});
 
     // Replace avatar in the player's lineup.
     const slot = body.slot ?? 0;
-    session.player.db.lineup.lineups[session.player.db.lineup.curIndex].avatarList[slot] = body.baseAvatarId;
+    const index = body.index ?? 1;
+
+    session.player.db.lineup.lineups[index].avatarList[slot] = body.baseAvatarId;
     await session.player.save();
 
     session.send(SyncLineupNotify, {
-        lineup: await session.player.getLineup(),
+        lineup: await session.player.getLineup(index),
         reasonList: [SyncLineupReason.SYNC_REASON_NONE]
     } as SyncLineupNotify);
 }
